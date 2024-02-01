7527be98c4d59652a7e10a2a4742d7a9091eb70b
thouay-baccam <thouay.baccam@laplateforme.io>
2024-02-01 10:00:58 +0100

    Enlever les database redundant

--- interface.py
+++ interface.py
@@ -2,7 +2,6 @@ import tkinter as tk
 from tkinter import ttk, messagebox
 from product import Product
 from category import Category
-from databaseconnect import DatabaseConnect
 
 class Interface:
     def __init__(self, db_connection):
